f790ab01e1f86f4bce69d206bdffa8612c7e5207
Nicola Aitken <naitken22@gmail.com>
2014-10-11 12:51:59 +0100

    turns left and right

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,9 +1,29 @@
 class Robot
 
-		def initialize(x, y, direction)
+		def initialize(x, y, orientation)
 			@position = "#{x},#{y}"
-			@direction = direction
+			@orientation = orientation
 		end
 
-		attr_reader :position, :direction
+		attr_reader :position, :orientation
+
+		def move(direction)
+			direction == "L" || "R" ? @orientation = turn(direction) : "go forward"
+		end
+
+private
+
+		def turn(direction)
+			case @orientation
+			when "N"
+				direction == "L" ? "W" : "E"
+			when "S"
+				direction == "L" ? "E" : "W"
+			when "E"
+				direction == "L" ? "N" : "S"
+			when "W"
+				direction == "L" ? "S" : "N"
+			end
+		end
+
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -3,11 +3,24 @@ require 'robot'
 
 describe 'robot' do 
 
+	let(:curiosity){Robot.new("1","1","N")}
+
 	context 'when created' do 
-		it 'has an initial location and direction' do
-			curiosity = Robot.new("1", "1", "N")
+		it 'has an initial location and orientation' do
 			expect(curiosity.position).to eq("1,1")
-			expect(curiosity.direction).to eq "N"
+			expect(curiosity.orientation).to eq "N"
+		end
+	end
+
+	context 'when given a command' do 
+		it 'turns left' do
+			curiosity.move("L")
+			expect(curiosity.orientation).to eq "W"
+		end
+
+		it 'turns right' do 
+			curiosity.move("R")
+			expect(curiosity.orientation).to eq "E"
 		end
 	end
 
