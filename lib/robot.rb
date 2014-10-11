3dbc8d27ffb14508c8c5bd32f2a836c23f0b49ed
Nicola Aitken <naitken22@gmail.com>
2014-10-11 14:02:55 +0100

    robot can move horizontally and vertically

--- lib/robot.rb
+++ lib/robot.rb
@@ -8,7 +8,7 @@ class Robot
 		attr_reader :position, :orientation
 
 		def move(direction)
-			direction == "L" || "R" ? @orientation = turn(direction) : "go forward"
+			direction =~ /\A(?:L|R|)\z/ ? @orientation = turn(direction) : go_forward
 		end
 
 private
@@ -25,5 +25,25 @@ private
 				direction == "L" ? "S" : "N"
 			end
 		end
+
+		def go_forward
+			case @orientation
+			when "N", "S"
+				move_vertically
+			when "E", "W"
+				move_horizontally
+			end
+		end
+
+		def move_vertically
+			vertical_position = @position.slice!(2).to_i
+			@position.insert(2,(@orientation == "N" ? vertical_position+=1 : vertical_position-=1).to_s)
+		end
+
+		def move_horizontally
+			horizontal_position = @position.slice!(0).to_i
+			@position.insert(0,(@orientation == "E" ? horizontal_position +=1 : horizontal_position -=1).to_s)
+		end
+
 
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -22,6 +22,31 @@ describe 'robot' do
 			curiosity.move("R")
 			expect(curiosity.orientation).to eq "E"
 		end
+
+		it 'moves North' do 
+			curiosity.move("F")
+			expect(curiosity.position).to eq("1,2")
+		end
+
+		it 'moves South' do 
+			curiosity = Robot.new("1","1","S")
+			curiosity.move("F")
+			expect(curiosity.position).to eq("1,0")
+		end
+
+		it 'moves East' do 
+			curiosity = Robot.new("1","1","E")
+			curiosity.move("F")
+			expect(curiosity.position).to eq("2,1")
+		end
+
+		it 'moves West' do 
+			curiosity = Robot.new("1","1","W")
+			curiosity.move("F")
+			expect(curiosity.position).to eq("0,1")
+		end
+
+
 	end
 
 end
